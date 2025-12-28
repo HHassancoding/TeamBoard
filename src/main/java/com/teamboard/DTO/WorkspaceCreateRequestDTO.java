4b5986051b41114e5c8926ec5118e8927566608c
Hamza Hassan <ZNAC270@live.rhul.ac.uk>
2025-12-28 14:19:11 +0000

    Implemented the WorkspaceDTO classes for reponse and request. Fixes #22

---
+++ src/main/java/com/teamboard/DTO/WorkspaceCreateRequestDTO.java
@@ -0,0 +1,14 @@
+package com.teamboard.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WorkspaceCreateRequestDTO {
+  private String name;
+  private String description;
+}
+
